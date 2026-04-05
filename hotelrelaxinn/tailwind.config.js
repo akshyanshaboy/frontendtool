4ec322d6e37d47e3d81e69d078d158bee2137c2a
akshyanshaboy <akshyanshacool@gmail.com>
2026-04-05 15:19:24 +0530

    Update tailwind.config.js

---
+++ hotelrelaxinn/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'sky-blue': '#87CEEB',
+      }
+    },
+  },
+  plugins: [],
+}
